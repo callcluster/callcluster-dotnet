65faf14a142f3d9f1b4b4e5efd210a0c1fdf857d
jisbruzzi <jose.sbru@gmail.com>
2020-06-23 20:58:30 -0300

    e492 - initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace callcluster_dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
