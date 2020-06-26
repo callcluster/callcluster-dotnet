baa046743876c8399924361d4b35da51f2dba7e9
jisbruzzi <jose.sbru@gmail.com>
2020-06-26 18:05:51 -0300

    6ac3 - Add simple tests

--- callcluster-dotnet.Tests/Test.cs
+++ callcluster-dotnet.Tests/Test.cs
@@ -2,15 +2,24 @@ using System;
 using Xunit;
 using callcluster_dotnet;
 using callcluster_dotnet.dto;
+using System.Linq;
 
 namespace callcluster_dotnet.Tests
 {
     public class Test
     {
         [Fact]
-        public async void SimpleSolution()
+        public async void SimpleProjectHasWriteLine()
         {
-            CallgraphDTO dto = await callcluster_dotnet.Program.Extract("./test-solutions/cli-sln/cli-sln.sln");
+            CallgraphDTO dto = await callcluster_dotnet.Program.Extract("../../../../test-solutions/cli-csproj/cli-csproj.csproj");
+            Assert.Contains(dto.functions,f => f.name=="System.Console.WriteLine(string)");
+        }
+
+        [Fact]
+        public async void SimpleProjectHasOneCall()
+        {
+            CallgraphDTO dto = await callcluster_dotnet.Program.Extract("../../../../test-solutions/cli-csproj/cli-csproj.csproj");
+            Assert.Single(dto.calls);
         }
     }
 }
