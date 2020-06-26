56a991370069156b7cd91090b7f2e9628c717979
jisbruzzi <jose.sbru@gmail.com>
2020-06-26 07:59:14 -0300

    6ac3 - Add complex project

---
+++ test-solutions/complex-project/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace complex_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void One(){
+            Two();
+            Three();
+        }
+
+        static void Two(){
+            Three();
+        }
+
+        static void Three(){
+            LoopHead();
+        }
+
+        static void LoopHead(){
+            LoopTail();
+        }
+
+        static void LoopTail(){
+            LoopHead();
+        }
+    }
+}
