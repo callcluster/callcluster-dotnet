8a1e8b1d857d94c4fab441fe7f2bc12e09f6cd34
jisbruzzi <jose.sbru@gmail.com>
2020-06-30 18:16:35 -0300

    6ac3 - fix constructors test so it compiles

---
+++ test-solutions/constructors/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
