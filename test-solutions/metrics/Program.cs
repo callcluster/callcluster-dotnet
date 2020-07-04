e6c065e330b8878c299f32595a929b555fc7cf62
jisbruzzi <jose.sbru@gmail.com>
2020-07-04 20:18:56 -0300

    6ac3 - Add project for testing metrics

---
+++ test-solutions/metrics/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace metrics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static int DoNormalComplexThings()
+        {
+            if(true)
+            {
+                return 10;
+            }
+            else
+            {
+                int b = 0;
+                if(false)
+                {
+                    int q = 1;
+                    q += 1;
+                    b = q;
+                }
+                for(int i=0;i<10;i++)
+                {
+                    b+=i;
+                }
+
+                return b;
+            }
+        }
+
+        static int DoNormalComplexThingsWithOneLessBranch()
+        {
+            int b = 0;
+            if(false)
+            {
+                int q = 1;
+                q += 1;
+                b = q;
+            }
+            for(int i=0;i<10;i++)
+            {
+                b+=i;
+            }
+            return b;
+        }
+
+        static int OneLiner()
+        {
+            int b = 0; return b;
+        }
+
+        static int TwoLiner()
+        {
+            int b = 0;
+            return b;
+        }
+
+        static int Returner()
+        {
+            return 10;
+        }
+
+    }
+}
