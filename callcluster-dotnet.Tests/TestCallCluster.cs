ec98762a98910f757b0f1c9aef570b564f5756d6
jisbruzzi <jose.sbru@gmail.com>
2020-11-04 11:28:05 -0300

    e5b8 - add tests and work on package resolution

--- callcluster-dotnet.Tests/TestCallCluster.cs
+++ callcluster-dotnet.Tests/TestCallCluster.cs
@@ -25,5 +25,46 @@ namespace callcluster_dotnet.Tests
 
             CallgraphAssert.CallPresent(dto,addClassMethod.Value,addTreeMethod.Value);
         }
+
+        [Fact]
+        public async void CallsToAnotherGenericMethod()
+        {
+            CallgraphDTO dto = await Utils.Extract("../callcluster-dotnet.sln");
+            CommunityDTO project = dto.community.Namespace("callcluster_dotnet");
+            
+            long? getCallDTOs = project
+            ?.Community("CallgraphCollector")
+            ?.Function("GetCallDTOs()",dto);
+
+            long? descendantsOf = project
+            ?.Community("Tree")
+            ?.Function("DescendantsOf(T)",dto);
+
+            CallgraphAssert.CallPresent(dto,getCallDTOs.Value,descendantsOf.Value);
+        }
+
+        [Fact]
+        public async void CallsFromTestingToExtractor()
+        {
+            CallgraphDTO dto = await Utils.Extract("../callcluster-dotnet.sln");
+            CommunityDTO extractor = dto.community.Community("callcluster-dotnet");
+            CommunityDTO tests = dto.community.Community("callcluster-dotnet.Tests");
+            
+            long? realExtract = extractor
+            .Namespace("callcluster_dotnet")
+            ?.Community("Program")
+            ?.Function("Extract(String)",dto);
+
+            long? testingExtract = tests
+            .Namespace("callcluster_dotnet")
+            ?.Community("Tests")
+            ?.Community("Utils")
+            ?.Function("Extract(String)",dto);
+
+            //var fromReal=dto.calls.Where(c=>c.from==realExtract.Value).ToList();
+            //var fromTesting=dto.calls.Where(c=>c.from==testingExtract.Value).ToList();
+
+            CallgraphAssert.CallPresent(dto,testingExtract.Value,realExtract.Value);
+        }
     }
 }
--- callcluster-dotnet/Program.cs
+++ callcluster-dotnet/Program.cs
@@ -8,6 +8,7 @@ using Newtonsoft.Json;
 using System.IO;
 using Microsoft.CodeAnalysis.MSBuild;
 using Microsoft.CodeAnalysis;
+using System.Runtime.Loader;
 
 namespace callcluster_dotnet
 {
@@ -20,11 +21,28 @@ namespace callcluster_dotnet
             File.WriteAllText(@"analysis.json", json);
         }
 
-        public static async Task<CallgraphDTO> Extract(string filePath){
-            if(MSBuildLocator.CanRegister){
-                MSBuildLocator.RegisterDefaults();
+        public static async Task<CallgraphDTO> Extract(string filePath)
+        {
+            if (!MSBuildLocator.IsRegistered && MSBuildLocator.CanRegister)
+            {
+                var instance = MSBuildLocator.RegisterDefaults();
+                AssemblyLoadContext.Default.Resolving += (assemblyLoadContext, assemblyName) =>
+                {
+                    var path = Path.Combine(instance.MSBuildPath, assemblyName.Name + ".dll");
+                    if (File.Exists(path))
+                    {
+                        return assemblyLoadContext.LoadFromAssemblyPath(path);
+                    }else{
+                        Console.WriteLine("Callcluster had problems loading an assembly, some references will not be loaded correctly: "+assemblyName);
+                        Console.WriteLine("Attempted to load it from "+path);
+                        Console.WriteLine("Please install "+assemblyName+" at "+ path);
+                    }
+
+                    return null;
+                };
+
             }
-            
+
             var _1 = typeof(Microsoft.CodeAnalysis.CSharp.Formatting.CSharpFormattingOptions);
             var _2 = typeof(Microsoft.CodeAnalysis.VisualBasic.VisualBasicDiagnosticFormatter);
 
@@ -40,11 +58,16 @@ namespace callcluster_dotnet
                         Console.WriteLine(workspaceFailedArgs.Diagnostic.Message);
                     }
                 };
-                if(Regex.Match(filePath,@".*\.sln$").Success){
+                if (Regex.Match(filePath, @".*\.sln$").Success)
+                {
                     walker.Visit(await workspace.OpenSolutionAsync(filePath));
-                }else if(Regex.Match(filePath,@".*\.csproj$").Success){
+                }
+                else if (Regex.Match(filePath, @".*\.csproj$").Success)
+                {
                     walker.Visit(await workspace.OpenProjectAsync(filePath));
-                }else{
+                }
+                else
+                {
                     Console.WriteLine("Not a solution or a project file.");
                     return null;
                 }
